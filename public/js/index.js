6744547c82258f85f459ce69888ee4db855a9a5d
Suspiciusduck <bobbycit2@gmail.com>
2021-03-23 08:42:46 +0100

    Commit

--- public/js/index.js
+++ public/js/index.js
@@ -19,18 +19,5 @@ fetchProducts().then(data => {
   const formatter = new Intl.NumberFormat("fr-FR", { style: "currency", currency: "EUR" }); // Formatte un nombre en devise
 
   // Crée un template pour chaque élément de l'array
-  const teddies = data.map(teddie => `
-    <div class="col-sm-6 text-center mb-4">
-      <div class="card">
-        <img class="card-img-top" src="${teddie.imageUrl}" alt="Appareil photo ${teddie.name}">
-        <div class="card-body">
-          <a href="pages/product.html?_id=${teddie._id}" class="text-dark text-decoration-none"><h3 class="card-title stretched-link">${teddie.name}</h3></a>
-          <p class="card-text">${teddie.description}</p>
-          <p class="btn btn-primary text-monospace mb-0">${formatter.format(teddie.price / 100)}</p>
-        </div>
-      </div>
-    </div>
-  `).join(""); // Assemble les templates
 
-  container.innerHTML += `<div class="d-flex flex-wrap mx-auto products">${teddies}</div>`; // Ajoute le template
 });
